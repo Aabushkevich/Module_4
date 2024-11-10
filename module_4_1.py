eafb114c16dbabbe0d01b13597d65be4f1e2bc93
Aabushkevich <abushkevich9063@gmail.com>
2024-11-10 20:26:54 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import fake_math as func1
+import true_math as func2
+
+result1 = func1.fake_divide(69,3)
+result2 = func1.fake_divide(3,0)
+result3 = func2.true_divide(49,7)
+result4 = func2.true_divide(15,0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
